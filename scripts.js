dd355c1b4ad89b386c36603fe567770cc758cf79
GagosideForMayor <77865272+GagosideForMayor@users.noreply.github.com>
2024-03-13 09:29:24 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Array of player objects (You can populate this dynamically from your Java backend)
+    // Array of player objects with their stats
     var players = [
-        { name: "Player1", jerseyNumber: 10, rating: 80, position: "Forward" },
-        { name: "Player2", jerseyNumber: 5, rating: 75, position: "Midfielder" },
-        // Add more players here
+        { name: "Govin G", jerseyNumber: 0, position: "DEF/MID/FWD", rating: 83 },
+        { name: "Ethan M", jerseyNumber: 1, position: "GK", rating: 85 },
+        { name: "Cooper B", jerseyNumber: 5, position: "DEF", rating: 80 },
+        { name: "Eashan K", jerseyNumber: 6, position: "DEF", rating: 78 },
+        { name: "Liang Yu T", jerseyNumber: 7, position: "MID/FWD", rating: 82 },
+        { name: "Nikush R", jerseyNumber: 9, position: "DEF", rating: 75 },
+        { name: "Ryan C", jerseyNumber: 13, position: "DEF/MID/FWD", rating: 82 },
+        { name: "Camillo V", jerseyNumber: 16, position: "FWD", rating: 85 },
+        { name: "Ryan H", jerseyNumber: 17, position: "MID/FWD", rating: 86 },
+        { name: "Oskar B", jerseyNumber: 20, position: "FWD", rating: 83 },
+        { name: "Cashel C", jerseyNumber: 21, position: "GK/CB/FWD", rating: 78 },
+        { name: "Lochlan F", jerseyNumber: 22, position: "DEF", rating: 85 },
+        { name: "Bobby F", jerseyNumber: 24, position: "CB", rating: 81 },
+        { name: "Sebastian D", jerseyNumber: 26, position: "MID/FWD", rating: 81 },
+        { name: "Areeb J", jerseyNumber: 28, position: "FWD", rating: 85 },
+        { name: "Daniel N", jerseyNumber: 35, position: "MID/FWD", rating: 85 },
+        { name: "Ricardo C", jerseyNumber: 72, position: "DEF/MID", rating: 86 },
+        { name: "Yi-Jay K", jerseyNumber: 95, position: "DEF/MID", rating: 82 },
+        { name: "Mori N", jerseyNumber: 111, position: "MID/FWD", rating: 85 },
+        { name: "Dani M", jerseyNumber: 33, position: "MID/FWD", rating: 91 },
+        { name: "Leo R", jerseyNumber: 30, position: "DEF", rating: 85 }
     ];
 
     // Populate the dropdown menu with player options
